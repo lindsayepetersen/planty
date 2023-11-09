411460ad192c7a7a7041a97aefa594bc3b155932
[lindsayepetersen] <lindsayepetersen@gmail.com>
2023-11-08 21:36:47 -0700

    added basic navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { mainNavbarItems } from './navbarItems';
+
+const drawerWidth = 220;
+
+const Navbar = () => {
+  return (
+    <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+            backgroundColor: '#91c499',
+            color: '#eee',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {mainNavbarItems.map((text, index) => (
+            <ListItem key={text.id} disablePadding>
+              <ListItemButton>
+                <ListItemIcon sx={{ color: '#eee' }}>
+                  {text.icon}
+                </ListItemIcon>
+                <ListItemText primary={text.label} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+  )
+}
+
+export default Navbar;
